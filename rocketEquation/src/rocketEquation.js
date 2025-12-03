5fabb29aecb636a98303e830fe1bb96d21fcebff
Karthik Dasari <karthikdasari@Karthiks-MacBook-Pro.local>
2025-12-03 10:47:20 +0530

    part -2 completes

--- rocketEquation/src/rocketEquation.js
+++ rocketEquation/src/rocketEquation.js
@@ -1,13 +1,19 @@
-export const fuelRequirment = (massInStirng) => {
-  const mass = parseInt(massInStirng)
-  return Math.floor(mass / 3) - 2;
+export const fuelRequirment = (mass) => {
+  const requiredFuel = Math.floor(mass / 3) - 2
+  if(requiredFuel <= 0) {
+    return 0;
+  }
+  return requiredFuel + fuelRequirment(requiredFuel);
 };
 
 export const masses = (modulesMasses) => {
   const requirdFuelForModules = modulesMasses.map((mass) =>
     fuelRequirment(mass)
   );
-  console.log(modulesMasses)
-  console.log(requirdFuelForModules)
   return requirdFuelForModules.reduce((acc, eachModule) => acc + eachModule, 0);
 };
+
+console.clear();
+const  input = Deno.readTextFileSync("src/input.txt").split("\n");
+console.log(input)
+console.log(masses(input))
--- rocketEquation/test/rocketEquation_test.js
+++ rocketEquation/test/rocketEquation_test.js
@@ -2,25 +2,21 @@ import { assertEquals } from "jsr:@std/assert";
 import { fuelRequirment, masses } from "../src/rocketEquation.js";
 
 Deno.test("simple case", () => {
-  assertEquals(fuelRequirment(12), 2)
-})
+  assertEquals(fuelRequirment(12), 2);
+});
 
 Deno.test("simple case", () => {
-  assertEquals(fuelRequirment(14), 2)
-})
+  assertEquals(fuelRequirment(14), 2);
+});
 
 Deno.test("A little heavy mass", () => {
-  assertEquals(fuelRequirment(1969), 654)
-})
+  assertEquals(fuelRequirment(1969), 966);
+});
 
 Deno.test("heavy mass", () => {
-  assertEquals(fuelRequirment(100756), 33583)
-})
-
-Deno.test("multiple modules mass", () => {
-  assertEquals(masses([12, 14, 1969, 100756]), 33583 + 654 + 2 +2)
-})
-
-Deno.test("multiple modules mass", () => {
-  assertEquals(masses([12, 14, 1969, 100756]), 33583 + 654 + 2 +2)
+  assertEquals(fuelRequirment(100756), 50346);
 });
+
+Deno.test("multiple modules mass", () => {
+  assertEquals(masses([12, 14, 1969, 100756]), 50346 + 966 + 2 +2)
+})
