982a2e15f1663a3aed4628b3e18b7755fd483729
Karthik Dasari <karthikdasari@Karthiks-MacBook-Pro.local>
2025-12-03 17:53:18 +0530

    day 3 completed

--- day3.js
+++ day3.js
@@ -19,12 +19,13 @@ const changePosition = (instruction, wire1) => {
   return wire1;
 };
 
-const exectuteOneWire = (instructions, path) =>{
+const exectuteOneWire = (instructions, path) => {
   return instructions.map((instruction) => changePosition(instruction, path));
-}
+};
 
-const isEqual = (point1, point2) =>
-  point1[0] === point2[0] && point1[1] === point2[1];
+const isEqual = (point1, point2) => {
+  return point1[0] === point2[0] && point1[1] === point2[1];
+};
 
 const intersectionPoints = (wire1, wire2) => {
   const commonPoints = [];
@@ -38,29 +39,60 @@ const intersectionPoints = (wire1, wire2) => {
   return commonPoints;
 };
 
-const manHattanDist = (point1, point2) => 
+const manHattanDist = (point1, point2) =>
   Math.abs(point1[0] - point2[0]) + Math.abs(point1[1] - point2[1]);
 
 const closetPointWithCentre = (points) => {
   // let closeDistance = Infinity;
-   return  points.reduce((closeDistance, point) => {
-    const currentPointDist = manHattanDist(point, [0,0])
-    if(closeDistance > currentPointDist) {
-      closeDistance = currentPointDist
+  return points.reduce((closeDistance, point) => {
+    const currentPointDist = manHattanDist(point, [0, 0]);
+    if (closeDistance > currentPointDist) {
+      closeDistance = currentPointDist;
     }
     return closeDistance;
-  }, Infinity)
+  }, Infinity);
+};
 
-}
+const stepsToReachFromCentre = (points, wire1, wire2) => {
+  const steps = [];
+  for (let i = 0; i < points.length; i++) {
+    const wire1Steps = wire1.indexOf(points[i]);
+  }
+};
+
+const countStepsFromPoint = (point, path) => {
+  for (let i = 0; i < path.length; i++) {
+    if (isEqual(point, path[i])) {
+      return i + 1;
+    }
+  }
+  return 0;
+};
+
+const countSteps = (intersectPoints, wire1, wire2) => {
+  // console.log(intersectPoints);
+  const totalSteps = intersectPoints.map((point) => {
+    const steps1 = countStepsFromPoint(point, wire1);
+    const steps2 = countStepsFromPoint(point, wire2);
+    // console.log(steps1, steps2);
+    return steps1 + steps2;
+  });
+  return totalSteps;
+};
 
 const main = () => {
   const wiresInstruction = Deno.readTextFileSync("day3input.txt").split("\n");
-  const wire1 = exectuteOneWire(wiresInstruction[0].split(','), []);
+  // const wiresInstruction = ["R8,U5,L5,D3", "U7,R6,D4,L4"];
+  const wire1 = exectuteOneWire(wiresInstruction[0].split(","), [])[0];
   wirePos = [0, 0];
-  const wire2 = exectuteOneWire(wiresInstruction[1].split(","), []);
-  const intersectPoints = intersectionPoints(wire1[0], wire2[0]);
-  const closetPoint =  closetPointWithCentre(intersectPoints);
-  console.log(closetPoint)
+  const wire2 = exectuteOneWire(wiresInstruction[1].split(","), [])[0];
+  const intersectPoints = intersectionPoints(wire1, wire2);
+  const totalSteps = countSteps(intersectPoints, wire1, wire2);
+  // console.log("dsk", totalSteps);
+  console.log(Math.min(...totalSteps));
+
+  // const closetPoint =  closetPointWithCentre(intersectPoints);
+  // console.log(closetPoint)
 };
 console.clear();
 
