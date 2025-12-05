c4ada78eb09b3f733eea9606fbf86c9a5ffa6746
Karthik Dasari <karthikdasari@Karthiks-MacBook-Pro.local>
2025-12-06 00:24:59 +0530

    day 8 part 2 completed

--- decodeImage/decodeImage.js
+++ decodeImage/decodeImage.js
@@ -1,46 +1,51 @@
-const WIDTH = 25;
-const TALL = 6;
+import { chunk, sortBy } from "jsr:@std/collections";
 
-const countOfEachRows = (layers) => {
-  return layers.map((layer) => {
-    layer.sort();
-    const firstOccurence = layer.indexOf("0");
-    const lastOccurence = layer.lastIndexOf("0");
-    return lastOccurence - firstOccurence + 1;
-  });
-};
+const frequency = (array) =>
+  array.reduce((table, target) => {
+    table[target] = (table[target] || 0) + 1;
+    return table;
+  }, {});
 
-const splitToLayers = (input) => {
-  const layers = [];
-  let index = 0;
-  while (index < input.length) {
-    const x = input.slice(index, index + (WIDTH * TALL));
-    console.log(x);
-    layers.push(x);
-    index = index + (WIDTH * TALL);
+const countOfEachRows = (layers) => layers.map((layer) => frequency(layer));
+
+const splitToLayers = (input, width, tall) => chunk(input, width * tall);
+
+const rowHasMinZeroes = (frequenices) => sortBy(frequenices, (fre) => fre["0"]);
+
+const decodeImage = (layers) => {
+  const output = [];
+  for (let x = 0; x < layers[0].length; x++) {
+    let y = 0;
+    while (layers[y][x] === "2") {
+      y++;
+    }
+    const emoji = layers[y][x] === "1" ? "⬜️" : "  ";
+    output.push(emoji);
   }
-  return layers;
+  console.log(output);
+  return output;
 };
 
-const multiplyOneAndTwo = (layer) => {
-  const firstOfOne = layer.indexOf("1");
-  const lastOfOne = layer.lastIndexOf("1");
-  const firstOfTwo = layer.indexOf("2");
-  const lastOfTwo = layer.lastIndexOf("2");
-  console.log((lastOfOne - firstOfOne + 1) * (lastOfTwo - firstOfTwo + 1))
-}
+// const part1 = (input) => {
+//   const frequencyOfDigits = countOfEachRows(layers);
+//   const frequencyInSort = rowHasMinZeroes(frequencyOfDigits);
+//   return frequencyInSort[0]["1"] * frequencyInSort[0]["2"];
+// };
+
+const part2 = (input, width, tall) => {
+  const layers = splitToLayers(input, width, tall);
+  return decodeImage(layers);
+};
+
+console.clear();
 
 const main = () => {
   const input = Deno.readTextFileSync("decodeImage/input.txt").split("");
-  const layers = splitToLayers(input);
-  const zeroesInlayers = countOfEachRows(layers);
-  console.log(zeroesInlayers);
-  const min = Math.min(...zeroesInlayers);
-  console.log(zeroesInlayers.indexOf(min));
-  const sorted = layers[zeroesInlayers.indexOf(min)].toSorted();
-  console.log(sorted);
-  const multiply = multiplyOneAndTwo(sorted);
+  const width = 25;
+  const tall = 6;
+  const output = part2(input, width, tall);
+  const grid = chunk(output, width);
+  console.log(grid.join("\n"));
 };
 
 main();
-// console.log(layers);
